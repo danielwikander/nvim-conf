15df73135afd74089e3016a876f5b99ca7baf9bf
Daniel Wikander <daniewik@axis.com>
2023-06-22 15:20:54 +0200

    remove conflicting keymap

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -4,18 +4,15 @@ return
     dependencies = 'nvim-lua/plenary.nvim',
     keys = {
         { '<C-g>',     '<CMD>DiffviewOpen<CR>',  mode = { 'n' } },
-        { '<leader>4', '<CMD>DiffviewClose<CR>', mode = { 'n' } }
     },
     opts = {
         keymaps = {
             view = {
                 ['<C-g>'] = '<CMD>DiffviewClose<CR>',
-                ['<leader>4'] = '<CMD>DiffviewClose<CR>',
                 ['<leader>1'] = '<CMD>DiffviewToggleFiles<CR>',
             },
             file_panel = {
                 ['<C-g>'] = '<CMD>DiffviewClose<CR>',
-                ['<leader>4'] = '<CMD>DiffviewClose<CR>',
                 ['<leader>1'] = '<CMD>DiffviewToggleFiles<CR>',
             }
         }
