982f98269cea7c8e64d34fc561825b4974404282
Daniel Wikander <daniel.wikander@gmail.com>
2023-06-30 10:54:30 +0200

    Change lazygit keybinds

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -3,16 +3,16 @@ return
     'sindrets/diffview.nvim',
     dependencies = 'nvim-lua/plenary.nvim',
     keys = {
-        { '<C-g>', '<CMD>DiffviewOpen<CR>', desc = 'Open diffview' },
+        { '<leader>8', '<CMD>DiffviewOpen<CR>', desc = 'Open diffview' },
     },
     opts = {
         keymaps = {
             view = {
-                ['<C-g>'] = '<CMD>DiffviewClose<CR>',
+                ['<leader>8'] = '<CMD>DiffviewClose<CR>',
                 ['<leader><tab>'] = '<CMD>DiffviewToggleFiles<CR>',
             },
             file_panel = {
-                ['<C-g>'] = '<CMD>DiffviewClose<CR>',
+                ['<leader>8'] = '<CMD>DiffviewClose<CR>',
                 ['<leader><tab>'] = '<CMD>DiffviewToggleFiles<CR>',
             }
         }
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,8 +1,8 @@
 return {
     'akinsho/toggleterm.nvim',
     keys = {
-        { '<C-t>',     '<cmd>ToggleTerm<cr>',            mode = { 'n', 't' }, desc = 'Toggle terminal' },
-        { '<leader>g', '<cmd>lua _lazygit_toggle()<CR>', mode = { 'n', 't' }, desc = 'Toggle lazygit' },
+        { '<C-t>', '<cmd>ToggleTerm<cr>',            mode = { 'n', 't' }, desc = 'Toggle terminal' },
+        { '<C-g>', '<cmd>lua _lazygit_toggle()<CR>', mode = { 'n', 't' }, desc = 'Toggle lazygit' },
     },
     config = function()
         require('toggleterm').setup({
@@ -20,7 +20,8 @@ return {
             on_open = function(term)
                 vim.cmd("startinsert!")
                 vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
-                vim.api.nvim_buf_set_keymap(term.bufnr, "n", "<leader>g", "<cmd>close<CR>", { noremap = true, silent = true })
+                vim.api.nvim_buf_set_keymap(term.bufnr, "n", "<leader>g", "<cmd>close<CR>",
+                    { noremap = true, silent = true })
             end,
             -- function to run on closing the terminal
             on_close = function()
