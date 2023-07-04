9bd8ffa7a5bf54cf48c615867c1369ba6f1e7de8
Daniel Wikander <daniel@preferit.se>
2023-07-04 13:49:04 +0200

    Change diagnostic signs for error and info

--- lua/config/opt.lua
+++ lua/config/opt.lua
@@ -59,14 +59,12 @@ vim.diagnostic.config({
     }
 })
 
-
-
 vim.fn.sign_define("DiagnosticSignError",
-    { text = "▲", texthl = "DiagnosticSignError" })
+    { text = "󰜋", texthl = "DiagnosticSignError" })
 vim.fn.sign_define("DiagnosticSignWarn",
     { text = "▲", texthl = "DiagnosticSignWarn" })
 vim.fn.sign_define("DiagnosticSignInfo",
-    { text = "⚑", texthl = "DiagnosticSignInfo" })
+    { text = "", texthl = "DiagnosticSignInfo" })
 vim.fn.sign_define("DiagnosticSignHint",
     { text = "⚑", texthl = "DiagnosticSignHint" })
 
