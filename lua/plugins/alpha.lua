95d0a0e997ea1e8475c2a09b62c5a37cf587abcc
Daniel Wikander <daniewik@axis.com>
2023-07-18 14:22:35 +0200

    Update alpha footer

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,6 +1,7 @@
 return {
     'goolord/alpha-nvim',
     event = 'VimEnter',
+    cmd = 'Alpha',
     config = function()
         local alpha = require 'alpha'
         local headers = {
@@ -27,35 +28,35 @@ return {
         local dashboard = require 'alpha.themes.dashboard'
         dashboard.section.header.val = headers.nvim_1
         dashboard.section.buttons.val = {
-            dashboard.button('p', '  Find File', ':Telescope find_files<CR>'),
+            dashboard.button('ctrl p', '  Find File', ':Telescope find_files<CR>'),
+            dashboard.button('spc p', '󰷾  Find Text', ':Telescope live_grep<CR>'),
             dashboard.button('a', '  New file', ':ene <BAR> startinsert <CR>'),
             dashboard.button('r', '󰄉  Recent Files', ':Telescope oldfiles<CR>'),
-            dashboard.button('t', '󰷾  Find Text', ':Telescope live_grep<CR>'),
             dashboard.button('c', '  Configuration', ':e $MYVIMRC<CR>:cd %:h<CR>'),
             dashboard.button('u', '  Update Plugins', ':Lazy update<CR>'),
             dashboard.button('q', '  Quit Neovim', ':qa!<CR>'),
         }
 
-        local footer = function()
-            local version = 'v.' .. vim.version().major .. '.' .. vim.version().minor .. '.' .. vim.version().patch
-            local lazy_ok, lazy = pcall(require, 'lazy')
-            if lazy_ok then
-                local total_plugins = '         ' .. lazy.stats().count .. ' Plugins'
-                local startuptime = (math.floor(lazy.stats().startuptime * 100 + 0.5) / 100)
-                return version .. total_plugins .. '         ' .. startuptime .. 'ms'
-            else
-                return version
-            end
-        end
+        dashboard.config.opts.leader = "ABC"
+        dashboard.config.opts.noautocmd = true
+        dashboard.opts.noautocmd = true
+        dashboard.opts.leader = 'ABC'
+
+        alpha.setup(dashboard.opts)
+
         vim.api.nvim_create_autocmd('User', {
             pattern = 'LazyVimStarted',
             callback = function()
-                dashboard.section.footer.val = footer()
+                local version = 'v.' .. vim.version().major .. '.' .. vim.version().minor .. '.' .. vim.version().patch
+                local stats = require('lazy').stats()
+                local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+                dashboard.section.footer.val = '   Neovim ' ..
+                    version .. '       ' ..
+                    stats.count .. ' plugins' .. '        ' ..
+                    ms .. 'ms '
+                pcall(vim.cmd.AlphaRedraw)
             end,
-            desc = 'Footer for Alpha',
+            desc = 'Footer for alpha'
         })
-
-        dashboard.opts.opts.noautocmd = false
-        alpha.setup(dashboard.opts)
     end
 }
