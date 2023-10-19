305d0f366148b4eaf5bb28ac17376e192651d1d3
Daniel Wikander <daniel@preferit.se>
2023-10-19 11:05:01 +0200

    Add ZenMode (with twilight), remove brackets from dashboard shortcuts

---
+++ lua/plugins/zen.lua
@@ -0,0 +1,26 @@
+return
+{
+    "folke/zen-mode.nvim",
+    keys = {
+        { '<leader>z', '<cmd>ZenMode<cr>', mode = 'n', desc = 'Toggle ZenMode' },
+    },
+    opts = {
+        window = {
+            backdrop = 1,             -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
+            options = {
+                signcolumn = "no",    -- disable signcolumn
+                cursorcolumn = false, -- disable cursor column
+            },
+        },
+        plugins = {
+            gitsigns = {
+                enabled = false,
+            },
+            alacritty = {
+                enabled = false,
+                font = "18",
+            },
+        }
+
+    }
+}
