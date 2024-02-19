0498d4c9e7021422738065121b0e695d67530abd
Daniel Wikander <daniel.wikander@zetadisplay.com>
2024-02-19 11:32:35 +0100

    Fix gx breaking changes

--- lua/plugins/gx.lua
+++ lua/plugins/gx.lua
@@ -1,6 +1,18 @@
 return {
   'chrishrb/gx.nvim',
-  event = { 'BufEnter' },
+  keys = {
+    {
+      'gx',
+      '<cmd>Browse<cr>',
+      mode = { 'n', 'x' },
+      desc = 'Open link in browser',
+    },
+  },
+  init = function ()
+    vim.g.netrw_nogx = 1 -- disable netrw gx
+  end,
+  submodules = false, -- not needed, submodules are required only for tests
+  cmd = { 'Browse' },
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = true, -- default settings
 }
