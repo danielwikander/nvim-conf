36a47a0877a13ed60ee59ab4826c71df5bd7cb13
Daniel Wikander <daniel.wikander@zetadisplay.com>
2024-03-19 12:56:37 +0100

    Add git hunk reset bind

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -6,6 +6,7 @@ return {
     { '<leader>w', '<cmd>Gitsigns preview_hunk_inline<CR>', desc = 'Preview hunk inline' },
     { '<leader>W', '<cmd>Gitsigns preview_hunk<CR>', desc = 'Preview hunk' },
     { '<leader>tb', '<cmd>Gitsigns toggle_current_line_blame<CR>', desc = 'Toggle git blame inline' },
+    { '<leader>gr', '<cmd>Gitsigns reset_hunk<CR>', desc = 'Reset git hunk' },
   },
   config = function()
     require('gitsigns').setup()
