159d25226104d640632278e50fb5c6e1c804d7e8
Daniel Wikander <daniel.wikander@zetadisplay.com>
2024-05-16 18:48:29 +0200

    Add new tint ignore pattern

--- lua/plugins/tint.lua
+++ lua/plugins/tint.lua
@@ -6,7 +6,18 @@ return {
       tint = -60, -- Darken colors, use a positive value to brighten
       saturation = 0.3, -- Saturation to preserve
       -- tint_background_colors = true,  -- Tint background portions of highlight groups
-      -- highlight_ignore_patterns = { "WinSeparator", "Status.*" },  -- Highlight group patterns to ignore, see `string.find`
+      highlight_ignore_patterns = {
+        'WinSeparator',
+        'Status.*',
+        'NeoTreeIndentMarker',
+        'NeoTreeDimText',
+        'NeoTreeDotFile',
+        'NeoTreeFadeText1',
+        'NeoTreeFadeText2',
+        'NeoTreeFileStats',
+        'NeoTreeMessage',
+        'EndOfBuffer',
+      }, -- Highlight group patterns to ignore, see `string.find`
       -- window_ignore_function = function(winid)
       --   local bufid = vim.api.nvim_win_get_buf(winid)
       --   local buftype = vim.api.nvim_buf_get_option(bufid, "buftype")
