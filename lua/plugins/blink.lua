e062c6aa7f463668db98a9987cc4fbc226ea94f9
Daniel Wikander <daniel@preferit.se>
2024-12-23 17:18:17 +0100

    Update blink config

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -2,17 +2,38 @@ return {
   'saghen/blink.cmp',
   lazy = false, -- lazy loading handled internally
   dependencies = 'rafamadriz/friendly-snippets',
-  version = 'v0.*',
+  version = '*',
   opts = {
-    highlight = {
-      use_nvim_cmp_as_default = true,
-    },
-    nerd_font_variant = 'mono',
+    -- highlight = {
+    -- use_nvim_cmp_as_default = true,
+    -- },
+    -- nerd_font_variant = 'mono',
     -- experimental auto-brackets support
-    accept = { auto_brackets = { enabled = true } },
+    -- accept = { auto_brackets = { enabled = true } },
     -- experimental signature help support
-    trigger = { signature_help = { enabled = true } },
-    draw = { treesitter = true },
+    -- trigger = { signature_help = { enabled = true } },
+    -- signature = { enabled = true },
+    -- completion = {
+    --   menu = {
+    --     draw = {
+    -- treesitter = true,
+    -- components = {
+    -- kind_icon = {
+    --   ellipsis = false,
+    --   text = function(ctx)
+    --     local kind_icon, _, _ = require('mini.icons').get('lsp', ctx.kind)
+    --     return kind_icon
+    --   end,
+    --   -- Optionally, you may also use the highlights from mini.icons
+    --   highlight = function(ctx)
+    --     local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
+    --     return hl
+    --   end,
+    -- },
+    -- },
+    --   },
+    -- },
+    -- },
     keymap = {
       ['<C-e>'] = { 'hide' },
       ['<CR>'] = { 'accept', 'snippet_forward', 'fallback' },
