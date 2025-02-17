818f31a247f20f6b7ff4dbcb8dce97518271a94f
Daniel Wikander <daniel.wikander@gmail.com>
2025-02-17 13:32:41 +0100

    Update blink cfg

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -40,6 +40,17 @@ return {
         Copilot = '',
       },
     },
+    cmdline = {
+      keymap = {
+        ['<C-e>'] = { 'hide' },
+        ['<Tab>'] = { 'accept', 'fallback' },
+        ['<Up>'] = { 'select_prev', 'fallback' },
+        ['<C-k>'] = { 'select_prev', 'fallback' },
+        ['<C-j>'] = { 'show', 'select_next', 'fallback' },
+        ['<Down>'] = { 'select_next', 'fallback' },
+        ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+      },
+    },
     keymap = {
       ['<C-e>'] = { 'hide' },
       ['<CR>'] = { 'accept', 'snippet_forward', 'fallback' },
@@ -49,15 +60,6 @@ return {
       ['<C-j>'] = { 'show', 'select_next', 'fallback' },
       ['<Down>'] = { 'select_next', 'fallback' },
       ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
-      cmdline = {
-        ['<C-e>'] = { 'hide' },
-        ['<Tab>'] = { 'accept', 'fallback' },
-        ['<Up>'] = { 'select_prev', 'fallback' },
-        ['<C-k>'] = { 'select_prev', 'fallback' },
-        ['<C-j>'] = { 'show', 'select_next', 'fallback' },
-        ['<Down>'] = { 'select_next', 'fallback' },
-        ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
-      },
     },
   },
 }
